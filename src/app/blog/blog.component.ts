df79f19421746e5ae2d980f8f1181a63a09c6631
Hallada, Matthew <Matthew.Hallada@fepoc.com>
2019-09-26 15:34:34 -0400

    CSS changes

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,7 +6,7 @@ import { Posts } from './mock-posts';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
 
